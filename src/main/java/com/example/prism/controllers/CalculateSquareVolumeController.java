1539aa975b125412bd44e701d5f000185685fd11
bszymala <bszymala20@gmail.com>
2025-01-24 18:29:50 +0100

    added constructor and calculate method

--- src/main/java/com/example/prism/controllers/CalculateSquareVolumeController.java
+++ src/main/java/com/example/prism/controllers/CalculateSquareVolumeController.java
@@ -1,7 +1,38 @@
 package com.example.prism.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CalculateSquareVolumeController {
+    @FXML
     private TextField sideField;
+    @FXML
+    private Label resultLabel;
+
+    private double height;
+
+    public CalculateSquareVolumeController(double height) {
+        this.height = height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @FXML
+    protected void Calculate() {
+        if (sideField.getText().isEmpty() || Integer.parseInt(sideField.getText()) <= 0) {
+            System.out.println("Invalid side value. must be greater than 0");
+            return;
+        }
+
+        double side = Integer.parseInt(sideField.getText());
+        double area = side * side;
+        double volume = area * height;
+
+        resultLabel.setText(side + " * " + side + " * " + height + " = " + volume);
+        resultLabel.setVisible(true);
+    }
 }
